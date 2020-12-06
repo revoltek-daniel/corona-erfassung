826825508699a914d5c207358eb0f1934f3bd268
revoltek-daniel <github@revoltek.de>
2020-12-06 20:31:41 +0100

    add fields

--- src/Controller/Admin/ContactPersonCrudController.php
+++ src/Controller/Admin/ContactPersonCrudController.php
@@ -4,6 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\ContactPerson;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TelephoneField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ContactPersonCrudController extends AbstractCrudController
 {
@@ -12,14 +18,22 @@ class ContactPersonCrudController extends AbstractCrudController
         return ContactPerson::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            ChoiceField::new('salutation')->setChoices(['Herr' => 'mr', 'Frau' => 'mrs', 'Divers' => 'div']),
+            'firstname',
+            'lastname',
+            TextField::new('street')->hideOnIndex(),
+            TextField::new('streetnumber')->hideOnIndex(),
+            NumberField::new('zip')->hideOnIndex(),
+            TextField::new('city')->hideOnIndex(),
+            TelephoneField::new('phone')->hideOnIndex(),
+            EmailField::new('email')->hideOnIndex(),
+            'contacted',
+            DateTimeField::new('contactDate'),
+            'tested',
         ];
     }
-    */
 }
